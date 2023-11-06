0efc4f1364d277fb3704106f2625a2d14682b9e7
devbathani <bathanid888@gmail.com>
2023-11-06 18:26:00 +0530

    completer added

--- lib/otpless_flutter_web.dart
+++ lib/otpless_flutter_web.dart
@@ -1,5 +1,6 @@
 library otpless_flutter_web;
 
+import 'dart:async';
 import 'dart:js' as js;
 import 'package:flutter/foundation.dart';
 
@@ -7,12 +8,30 @@ class Otpless {
   /*
     triggers the openLoginPage function which will open the login page from javascript 
   */
+
   Future<dynamic> openLoginPage() async {
     if (kIsWeb) {
-      String? data;
-      await js.context.callMethod("openLoginPage", []);
-      js.context['callDartFunction'] = data;
-      return data;
+      final completer = Completer<dynamic>();
+
+      js.context.callMethod("openLoginPage", []);
+
+      // Define the Dart function
+      callDartFunction(String? message) {
+        if (message != null) {
+          completer.complete(message);
+        } else {
+          completer.completeError("Message is null");
+        }
+      }
+
+      // Assign the Dart function to the JavaScript context
+      js.context['callDartFunction'] = callDartFunction;
+
+      // Return the Future that will be completed when message is not null
+      return completer.future;
     }
+
+    // Return a Future that's already completed (in this case, you may want to return a different value or null)
+    return Future.value(null);
   }
 }
