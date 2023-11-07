347ee87d7efcca4493c387ecde65a2ea68a006cf
devbathani <bathanid888@gmail.com>
2023-11-07 14:50:06 +0530

    example update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:example/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:otpless_flutter_web/otpless_flutter_web.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +18,77 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String? name;
+  String? number;
+
+  //Define the instance
+  final _otplessFlutterPlugin = Otpless();
+
+  //************************************************************************* */
+  //This function will run the floater in the app which contains the WhatsApp button for Authentication
+  //************************************************************************* */
+
+  void otplessLoginPage() async {
+    await _otplessFlutterPlugin.openLoginPage().then((value) {
+      final data = value;
+      name = data["data"]["mobile"]["name"];
+      number = data["data"]["mobile"]["number"];
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    if (_otplessFlutterPlugin.getCodeForParams() != null) {
+      otplessLoginPage();
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          otplessLoginPage();
+        },
+        child: const Text(
+          "Login",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Name : $name",
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Number : $number",
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
